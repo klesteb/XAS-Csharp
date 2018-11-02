ef3ea60656be0529d0a28372c4da741fe4257084
Kevin L. Esteb <kevin@kesteb.us>
2018-11-02 16:01:29 -0700

    adding a new service

---
+++ ServiceSupervisor/Model/Manager.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ServiceSupervisor.Model {
+
+    public class Manager {
+
+        public Repositories Repository { get; set; }
+
+        public Manager(Repositories repository) {
+
+            this.Repository = repository;
+
+        }
+
+    }
+
+}
---
+++ ServiceSupervisor/Web/Services/Supervised.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using XAS.Model;
+using XAS.Model.Paging;
+using XAS.Core.Logging;
+using XAS.Core.Exceptions;
+using XAS.Core.Configuration;
+
+
+namespace ServiceSupervisor.Web.Services {
+
+    /// <summary>
+    /// A repository service.
+    /// </summary>
+    /// 
+    public class Supervised: ISupervised {
+
+        private readonly ILogger log = null;
+        private readonly Model.Manager manager = null;
+        private readonly IConfiguration config = null;
+        private readonly IErrorHandler handler = null;
+        private readonly Web.Services.Supervised service = null;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="config">An IConfiguration object.</param>
+        /// <param name="handler">An IErrorHandler object.</param>
+        /// <param name="logFactory">An ILoggerFactory object.</param>
+        /// 
+        public Supervised(IConfiguration config, IErrorHandler handler, ILoggerFactory logFactory) {
+
+            this.config = config;
+            this.handler = handler;
+
+            var key = config.Key;
+            var section = config.Section;
+
+            var repository = new Model.Repositories(config, handler, logFactory);
+
+            this.manager = new Model.Manager(repository);
+            this.service = new Web.Services.Supervised(config, handler, logFactory);
+            this.log = logFactory.Create(typeof(Supervised));
+
+            log.Trace("Initialized DinoService()");
+
+        }
+
+    }
+
+}
